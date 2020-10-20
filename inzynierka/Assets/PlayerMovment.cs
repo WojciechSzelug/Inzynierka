50d82db7fd3b15c23e5ede164265810a38c31205
WojciechSzelug <67273373+WojciechSzelug@users.noreply.github.com>
2020-10-20 14:22:03 +0200

    Add a player movement and delete game from assets

---
+++ inzynierka/Assets/PlayerMovment.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+
+    public float movespeed;
+    public Rigidbody2D rd;
+
+    private Vector2 moveDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY);
+    }
+
+    void Move()
+    {
+        rd.velocity = new Vector2(moveDirection.x * movespeed, moveDirection.y * movespeed);
+    }
+}
